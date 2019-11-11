1b0ddcb4afaad79d921dd1958e0ace400a50819a
awalford16 <awalford16@gmail.com>
2019-11-11 20:54:24 +0000

    And plotting for polynomial regression and error

--- pol_regression.py
+++ pol_regression.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import math
+import random
 
 # ---------- POLYNOMIAL REGRESSION FUNCTIONS ----------
 
@@ -37,13 +38,11 @@ def prediction(x_test, w, degree):
     return y
 
 # Calculate the RMSE of the polynomial regression model
-def eval_pol_regression(w, x, y, degree):
-    # Predict test values for y
-    y_hat = prediction(x, w, degree)
+def eval_pol_regression(x, y, y_hat, degree):
     error = 0
     for i, pred in enumerate(y_hat):
         # Calculate difference between predicted value and actual value
-        residual = y[i] - pred
+        residual = pred - y[i]
 
         # Get squared difference
         error += residual ** 2
@@ -80,12 +79,18 @@ def split_data(data):
 
 # ---------- VISUALISTATION ----------
 
+def select_color(i):
+    colors = ['r', 'g', 'c', 'm', 'y', 'b', 'k']
+
+    return colors[i]
+
+# Plot error graph
 def plot_error_graph(train_error, test_error):
     plt.figure()
-    plt.semilogy(range(1,11), train_error)
-    plt.semilogy(range(1,11), test_error)
+    plt.semilogy([0,1,2,3,4,5,10], train_error)
+    plt.semilogy([0,1,2,3,4,5,10], test_error)
     plt.legend(('RMSE on training set', 'RMSE on test set'))
-    plt.savefig('polynomial_evaluation.png')
+    plt.savefig('polynomial_error.png')
     plt.show()
 
 
@@ -96,22 +101,48 @@ def main():
     df = load_data()
     x_train, y_train, x_test, y_test = split_data(df)
 
+    train_error_array = np.zeros((7, 1)) 
+    test_error_array = np.zeros((7, 1)) 
+
+    # Build graph for plotting
+    plt.figure()
+
+    # Plot training and testing data
+    plt.plot(x_test, y_test, 'g')
+    plt.plot(x_train, y_train, 'bo')
+
     # Perform polynomial regression for powers 0 to 10
-    for i in [0, 1,2,3,4,5,10]:
-        if i != 0:
+    for i, degree in enumerate([0, 1,2,3,4,5,10]):
+        if degree != 0:
             # Calculate the coefficients based on the training values
-            w = pol_regression(x_train, y_train, i)
+            w = pol_regression(x_train, y_train, degree)
         else: 
             w = 1
 
+        # Make predictions based on training model
+        y_hat = prediction(x_test, w, degree)
+
+        # Plot predictions
+        plt.plot(x_test, y_hat, select_color(i))
+
         # Measure accuracy of model
         # RMSE of training set
-        train_error = eval_pol_regression(w, x_train, y_train, i)
+        train_error_array[i] = eval_pol_regression(x_train, y_train, y_hat, i)
 
         # RMSE of testing set
-        test_error = eval_pol_regression(w, x_test, y_test, i)
-        print("[Degree: {0}] - Train: {1:.4f}, Test: {2:.4f}".format(i, train_error, test_error))
+        test_error_array[i] = eval_pol_regression(x_test, y_test, y_hat, i)
+        # print("[Degree: {0}] - Train: {1:.4f}, Test: {2:.4f}".format(i, train_error, test_error))
+    
+    
+    # Set plot between 5 and -5
+    plt.ylim((-5, 5))
+    plt.xlim(-5,5)
 
+    plt.legend(('Training Points', 'Actual Values', '$x^{0}$', '$x^{1}$', '$x^{2}$', '$x^{3}$', '$x^{4}$', '$x^{5}$', '$x^{10}$'), loc = 'lower left')
+    plt.savefig('pol_regression.png')
+
+    # Plot error graph
+    plot_error_graph(train_error_array, test_error_array)
 
 
 if __name__ == "__main__":
